bazel-contrib/rules-template

Move resolved_toolchain into the main workspace (#48)

mylang/private/resolved_toolchain.bzl
@@ -1,0 +1,27 @@
+"""This module implements an alias rule to the resolved toolchain.
+"""
+
+DOC = """\
+Exposes a concrete toolchain which is the result of Bazel resolving the
+toolchain for the execution or target platform.
+Workaround for https://github.com/bazelbuild/bazel/issues/14009
+"""
+
+# Forward all the providers
+def _resolved_toolchain_impl(ctx):
+    toolchain_info = ctx.toolchains["//mylang:toolchain_type"]
+    return [
+        toolchain_info,
+        toolchain_info.default,
+        toolchain_info.mylanginfo,
+        toolchain_info.template_variables,
+    ]
+
+# Copied from java_toolchain_alias
+# https://cs.opensource.google/bazel/bazel/+/master:tools/jdk/java_toolchain_alias.bzl
+resolved_toolchain = rule(
+    implementation = _resolved_toolchain_impl,
+    toolchains = ["//mylang:toolchain_type"],
+    incompatible_use_toolchain_transition = True,
+    doc = DOC,
+)

aherrmann/rules_zig

zig/private/resolved_toolchain.bzl
@@ -1,0 +1,27 @@
+"""This module implements an alias rule to the resolved toolchain.
+"""
+
+DOC = """\
+Exposes a concrete toolchain which is the result of Bazel resolving the
+toolchain for the execution or target platform.
+Workaround for https://github.com/bazelbuild/bazel/issues/14009
+"""
+
+# Forward all the providers
+def _resolved_toolchain_impl(ctx):
+    toolchain_info = ctx.toolchains["//zig:toolchain_type"]
+    return [
+        toolchain_info,
+        toolchain_info.default,
+        toolchain_info.ziginfo,
+        toolchain_info.template_variables,
+    ]
+
+# Copied from java_toolchain_alias
+# https://cs.opensource.google/bazel/bazel/+/master:tools/jdk/java_toolchain_alias.bzl
+resolved_toolchain = rule(
+    implementation = _resolved_toolchain_impl,
+    toolchains = ["//zig:toolchain_type"],
+    incompatible_use_toolchain_transition = True,
+    doc = DOC,
+)