13df07415a60ccf9e7858696475084835bd5e265
Andreas Herrmann <andreash87@gmx.ch>
2023-06-08 08:01:57 +0200

    debug: integration test

--- zig/tests/integration_tests/integration_tests_runner.zig
+++ zig/tests/integration_tests/integration_tests_runner.zig
@@ -67,6 +67,7 @@ const BitContext = struct {
         if (self.bzlmod_enabled and !args.omit_bzlmod_flag) {
             argv[argc - 1] = "--enable_bzlmod";
         }
+        std.debug.print("\n{s}\n", .{argv});
         const result = try std.ChildProcess.exec(.{
             .allocator = std.testing.allocator,
             .argv = argv,
@@ -88,121 +89,121 @@ const BitContext = struct {
     }
 };
 
-test "zig_binary prints Hello World!" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "run", "//:binary" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-    try std.testing.expectEqualStrings("Hello World!\n", result.stdout);
-}
-
-test "succeeding zig_test passes" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "test", "//:test-succeeds" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-}
-
-test "failing zig_test fails" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "test", "//:test-fails" },
-        .print_on_error = false,
-    });
-    defer result.deinit();
-
-    // See https://bazel.build/run/scripts for Bazel exit codes.
-    try std.testing.expectEqual(std.ChildProcess.Term{ .Exited = 3 }, result.term);
-}
-
-test "target build mode defaults to Debug" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "run", "//:print_build_mode" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-    try std.testing.expectEqualStrings("Debug", result.stdout);
-}
-
-test "exec build mode defaults to Debug" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "build", "//:exec_build_mode" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-
-    var workspace = try std.fs.cwd().openDir(ctx.workspace_path, .{});
-    defer workspace.close();
-
-    const build_mode = try workspace.readFileAlloc(std.testing.allocator, "bazel-bin/exec_build_mode.out", 16);
-    defer std.testing.allocator.free(build_mode);
-    try std.testing.expectEqualStrings("Debug", build_mode);
-}
-
-test "target build mode can be set on the command line" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "run", "//:print_build_mode", "--@rules_zig//zig/settings:mode=release_small" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-    try std.testing.expectEqualStrings("ReleaseSmall", result.stdout);
-}
-
-test "exec build mode can be set on the command line" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "build", "//:exec_build_mode", "--@rules_zig//zig/settings:mode=release_small" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-
-    var workspace = try std.fs.cwd().openDir(ctx.workspace_path, .{});
-    defer workspace.close();
-
-    const build_mode = try workspace.readFileAlloc(std.testing.allocator, "bazel-bin/exec_build_mode.out", 16);
-    defer std.testing.allocator.free(build_mode);
-    try std.testing.expectEqualStrings("ReleaseSmall", build_mode);
-}
-
-test "can compile to target platform aarch64-linux" {
-    const ctx = try BitContext.init();
-
-    const result = try ctx.exec_bazel(.{
-        .argv = &[_][]const u8{ "build", "//:binary", "--platforms=:aarch64-linux" },
-    });
-    defer result.deinit();
-
-    try std.testing.expect(result.success);
-
-    var workspace = try std.fs.cwd().openDir(ctx.workspace_path, .{});
-    defer workspace.close();
-
-    const file = try workspace.openFile("bazel-bin/binary", .{});
-    defer file.close();
-
-    const elf_header = try std.elf.Header.read(file);
-    try std.testing.expectEqual(std.elf.EM.AARCH64, elf_header.machine);
-}
+//test "zig_binary prints Hello World!" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "run", "//:binary" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//    try std.testing.expectEqualStrings("Hello World!\n", result.stdout);
+//}
+//
+//test "succeeding zig_test passes" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "test", "//:test-succeeds" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//}
+//
+//test "failing zig_test fails" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "test", "//:test-fails" },
+//        .print_on_error = false,
+//    });
+//    defer result.deinit();
+//
+//    // See https://bazel.build/run/scripts for Bazel exit codes.
+//    try std.testing.expectEqual(std.ChildProcess.Term{ .Exited = 3 }, result.term);
+//}
+//
+//test "target build mode defaults to Debug" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "run", "//:print_build_mode" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//    try std.testing.expectEqualStrings("Debug", result.stdout);
+//}
+//
+//test "exec build mode defaults to Debug" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "build", "//:exec_build_mode" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//
+//    var workspace = try std.fs.cwd().openDir(ctx.workspace_path, .{});
+//    defer workspace.close();
+//
+//    const build_mode = try workspace.readFileAlloc(std.testing.allocator, "bazel-bin/exec_build_mode.out", 16);
+//    defer std.testing.allocator.free(build_mode);
+//    try std.testing.expectEqualStrings("Debug", build_mode);
+//}
+//
+//test "target build mode can be set on the command line" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "run", "//:print_build_mode", "--@rules_zig//zig/settings:mode=release_small" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//    try std.testing.expectEqualStrings("ReleaseSmall", result.stdout);
+//}
+//
+//test "exec build mode can be set on the command line" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "build", "//:exec_build_mode", "--@rules_zig//zig/settings:mode=release_small" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//
+//    var workspace = try std.fs.cwd().openDir(ctx.workspace_path, .{});
+//    defer workspace.close();
+//
+//    const build_mode = try workspace.readFileAlloc(std.testing.allocator, "bazel-bin/exec_build_mode.out", 16);
+//    defer std.testing.allocator.free(build_mode);
+//    try std.testing.expectEqualStrings("ReleaseSmall", build_mode);
+//}
+//
+//test "can compile to target platform aarch64-linux" {
+//    const ctx = try BitContext.init();
+//
+//    const result = try ctx.exec_bazel(.{
+//        .argv = &[_][]const u8{ "build", "//:binary", "--platforms=:aarch64-linux" },
+//    });
+//    defer result.deinit();
+//
+//    try std.testing.expect(result.success);
+//
+//    var workspace = try std.fs.cwd().openDir(ctx.workspace_path, .{});
+//    defer workspace.close();
+//
+//    const file = try workspace.openFile("bazel-bin/binary", .{});
+//    defer file.close();
+//
+//    const elf_header = try std.elf.Header.read(file);
+//    try std.testing.expectEqual(std.elf.EM.AARCH64, elf_header.machine);
+//}
 
 test "zig_binary result should not contain the output base path" {
     const ctx = try BitContext.init();
@@ -213,6 +214,7 @@ test "zig_binary result should not contain the output base path" {
     defer info_result.deinit();
 
     const output_base = std.mem.trim(u8, info_result.stdout, " \n");
+    std.debug.print("output_base {s}\n", .{output_base});
 
     const result = try ctx.exec_bazel(.{
         .argv = &[_][]const u8{ "build", "//:binary", "--@rules_zig//zig/settings:mode=debug" },
@@ -229,6 +231,30 @@ test "zig_binary result should not contain the output base path" {
 
     const file_content = try file.readToEndAlloc(std.testing.allocator, 1_000_000);
     defer std.testing.allocator.free(file_content);
+
+    //const binary_path = try std.fs.path.join(std.testing.allocator, &[_][]const u8{
+    //    ctx.workspace_path,
+    //    "bazel-bin/binary",
+    //});
+    //defer std.testing.allocator.free(binary_path);
+
+    const strings_cmd = try std.fmt.allocPrint(
+        std.testing.allocator,
+        "strings {s}/{s} | grep /",
+        .{ ctx.workspace_path, "bazel-bin/binary" },
+    );
+    defer std.testing.allocator.free(strings_cmd);
+
+    var strings_proc = std.ChildProcess.init(
+        &[_][]const u8{
+            "sh",
+            "-c",
+            strings_cmd,
+        },
+        std.testing.allocator,
+    );
+    try strings_proc.spawn();
+    _ = try strings_proc.wait();
 
     try std.testing.expectEqual(@as(?usize, null), std.mem.indexOf(u8, file_content, output_base));
 }
