06a9a6092c92a76370e24b15bd9306b169d463bd
Andreas Herrmann <andreash87@gmx.ch>
2023-07-08 15:17:50 +0200

    end-to-end test for direct data dependencies

---
+++ e2e/workspace/data-dependencies/main.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+test "read data file" {
+    var file = try std.fs.cwd().openFile("data-dependencies/data.txt", .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(std.testing.allocator, 4096);
+    defer std.testing.allocator.free(content);
+
+    try std.testing.expectEqualStrings("Hello World!\n", content);
+}
