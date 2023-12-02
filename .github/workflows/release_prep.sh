df1439a80ca73a1b80ef34bf22d9fbc0a28d8d93
Andreas Herrmann <andreash87@gmx.ch>
2023-12-02 11:59:51 +0100

    Refine release prep script

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,20 +5,21 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
+VERSION=${TAG:1}
 # The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_zig-${TAG:1}"
-ARCHIVE="rules_zig-$TAG.tar.gz"
+PREFIX="rules_zig-$VERSION"
+ARCHIVE="$PREFIX.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel 6
+## Using Bzlmod with Bazel >=6
 
 1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
 2. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(name = "rules_zig", version = "${TAG:1}")
+bazel_dep(name = "rules_zig", version = "$VERSION")
 \`\`\`
 
 ## Using WORKSPACE
@@ -27,13 +28,14 @@ Paste this snippet into your `WORKSPACE.bazel` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
 http_archive(
     name = "rules_zig",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/myorg/rules_zig/releases/download/${TAG}/${ARCHIVE}",
+    url = "https://github.com/aherrmann/rules_zig/releases/download/${TAG}/${ARCHIVE}",
 )
 EOF
 
 awk 'f;/--SNIP--/{f=1}' e2e/workspace/WORKSPACE.bazel
-echo "\`\`\`" 
+echo "\`\`\`"
