f36f320e00d154262d034965bb19afdcf87df4dd
Andreas Herrmann <andreash87@gmx.ch>
2023-12-26 12:04:30 +0100

    Test linkdeps on zig_library and zig_test

--- e2e/workspace/link-dependencies/static-library/main.zig
+++ e2e/workspace/link-dependencies/static-library/main.zig
@@ -6,3 +6,7 @@ pub fn main() !void {
     const three = add(1, 2);
     try std.io.getStdOut().writer().print("{d}\n", .{three});
 }
+
+test "One plus two equals three" {
+    try std.testing.expectEqual(@as(u8, 3), add(1, 2));
+}
