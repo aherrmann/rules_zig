1603fb0380b425bdd770764c4736b091814a20dd
Andreas Herrmann <andreash87@gmx.ch>
2024-01-28 18:21:36 +0100

    add binary run test

---
+++ e2e/workspace/runfiles-library/main.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const runfiles = @import("runfiles");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    var file = try std.fs.cwd().openFile("runfiles-library/data.txt", .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(allocator, 4096);
+    defer allocator.free(content);
+
+    try std.io.getStdOut().writer().print("data: {s}", .{content});
+}
+
+test "read data file" {
+    var file = try std.fs.cwd().openFile("runfiles-library/data.txt", .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(std.testing.allocator, 4096);
+    defer std.testing.allocator.free(content);
+
+    try std.testing.expectEqualStrings("Hello World!\n", content);
+}
