004615c76f81c8611c437fdfc5b47a5147fd7da6
Andreas Herrmann <andreash87@gmx.ch>
2024-01-28 20:11:34 +0100

    factor out getEnvVar

--- zig/runfiles/src/Runfiles.zig
+++ zig/runfiles/src/Runfiles.zig
@@ -4,18 +4,18 @@ const Self = @This();
 
 directory: []const u8,
 
-pub fn create(allocator: std.mem.Allocator) !Self {
-    const var_path: ?[]const u8 = std.process.getEnvVarOwned(allocator, "RUNFILES_DIR") catch |e| switch (e) {
+fn getEnvVar(allocator: std.mem.Allocator, key: []const u8) !?[]const u8 {
+    return std.process.getEnvVarOwned(allocator, key) catch |e| switch (e) {
         error.EnvironmentVariableNotFound => null,
         else => |e_| return e_,
     };
-    defer if (var_path) |val| allocator.free(val);
+}
 
-    if (var_path) |val| {
-        const runfiles_path = try std.fs.cwd().realpathAlloc(allocator, val);
-        return .{
-            .directory = runfiles_path,
-        };
+pub fn create(allocator: std.mem.Allocator) !Self {
+    if (try getEnvVar(allocator, "RUNFILES_DIR")) |value| {
+        defer allocator.free(value);
+        const runfiles_path = try std.fs.cwd().realpathAlloc(allocator, value);
+        return .{ .directory = runfiles_path };
     }
 
     var iter = try std.process.argsWithAllocator(allocator);
