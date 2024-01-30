19c846a8f66f0b64335b13670ce7933bf7330a0f
Andreas Herrmann <andreash87@gmx.ch>
2024-01-30 08:27:58 +0100

    Test location expansion on `env` attribute

--- e2e/workspace/location-expansion/main.zig
+++ e2e/workspace/location-expansion/main.zig
@@ -5,25 +5,34 @@ extern const rlocationpath: [*:0]const u8;
 extern const target: [*:0]const u8;
 extern const zig_target: [*:0]const u8;
 
-test "RLOCATIONPATH is set" {
+test "-DRLOCATIONPATH is set" {
     try std.testing.expectStringEndsWith(
         std.mem.sliceTo(rlocationpath, 0),
         "location-expansion/data.txt",
     );
 }
 
-test "TARGET is set" {
+test "-DTARGET is set" {
     try std.testing.expectEqualStrings(
         "//location-expansion:test",
         std.mem.sliceTo(target, 0),
     );
 }
 
-test "ZIG_TARGET is set" {
+test "-DZIG_TARGET is set" {
     const actual_target = try builtin.target.linuxTriple(std.testing.allocator);
     defer std.testing.allocator.free(actual_target);
     try std.testing.expectEqualStrings(
         actual_target,
         std.mem.sliceTo(zig_target, 0),
+    );
+}
+
+test "Env-var TARGET is set" {
+    const value = try std.process.getEnvVarOwned(std.testing.allocator, "TARGET");
+    defer std.testing.allocator.free(value);
+    try std.testing.expectEqualStrings(
+        "//location-expansion:test",
+        value,
     );
 }
