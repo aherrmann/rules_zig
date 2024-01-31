9319e191cd4e64bba8209d6f120207fea246f134
Andreas Herrmann <andreash87@gmx.ch>
2024-01-31 08:15:16 +0100

    Test runfiles on external repository dependency

--- e2e/workspace/runfiles-library/main.zig
+++ e2e/workspace/runfiles-library/main.zig
@@ -49,3 +49,25 @@ test "read data file" {
 
     try std.testing.expectEqualStrings("Hello World!\n", content);
 }
+
+test "resolve external dependency rpath" {
+    var r = try runfiles.Runfiles.create(std.testing.allocator);
+    defer r.deinit(std.testing.allocator);
+
+    const rpath = std.process.getEnvVarOwned(std.testing.allocator, "DEPENDENCY_DATA") catch |e| switch (e) {
+        error.EnvironmentVariableNotFound => return error.EnvironmentVariableDEPENDENCY_DATANotFound,
+        else => |e_| return e_,
+    };
+    defer std.testing.allocator.free(rpath);
+
+    const file_path = try r.rlocation(std.testing.allocator, rpath);
+    defer std.testing.allocator.free(file_path);
+
+    var file = try std.fs.cwd().openFile(file_path, .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(std.testing.allocator, 4096);
+    defer std.testing.allocator.free(content);
+
+    try std.testing.expectEqualStrings("Hello from dependency!\n", content);
+}
