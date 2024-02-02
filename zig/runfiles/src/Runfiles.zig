15bba45178b60a723de74713bb5b6a4f2d216719
Andreas Herrmann <andreash87@gmx.ch>
2024-02-02 09:00:58 +0100

    Factor out runfiles discovery

--- zig/runfiles/src/Runfiles.zig
+++ zig/runfiles/src/Runfiles.zig
@@ -14,31 +14,34 @@ fn getEnvVar(allocator: std.mem.Allocator, key: []const u8) !?[]const u8 {
     };
 }
 
-pub fn create(allocator: std.mem.Allocator) !Self {
+fn discoverRunfiles(allocator: std.mem.Allocator) ![]const u8 {
     if (try getEnvVar(allocator, "RUNFILES_DIR")) |value| {
         defer allocator.free(value);
-        const runfiles_path = try std.fs.cwd().realpathAlloc(allocator, value);
-        return .{ .directory = runfiles_path };
+        return try std.fs.cwd().realpathAlloc(allocator, value);
+    } else {
+        var iter = try std.process.argsWithAllocator(allocator);
+        defer iter.deinit();
+
+        const argv0 = iter.next() orelse
+            return error.Argv0Unavailable;
+
+        const check_path = try std.fmt.allocPrint(
+            allocator,
+            "{s}.runfiles",
+            .{argv0},
+        );
+        defer allocator.free(check_path);
+
+        var dir = std.fs.cwd().openDir(check_path, .{}) catch
+            return error.RunfilesNotFound;
+        dir.close();
+
+        return try std.fs.cwd().realpathAlloc(allocator, check_path);
     }
+}
 
-    var iter = try std.process.argsWithAllocator(allocator);
-    defer iter.deinit();
-
-    const argv0 = iter.next() orelse
-        return error.Argv0Unavailable;
-
-    const check_path = try std.fmt.allocPrint(
-        allocator,
-        "{s}.runfiles",
-        .{argv0},
-    );
-    defer allocator.free(check_path);
-
-    var dir = std.fs.cwd().openDir(check_path, .{}) catch
-        return error.RunfilesNotFound;
-    dir.close();
-
-    const runfiles_path = try std.fs.cwd().realpathAlloc(allocator, check_path);
+pub fn create(allocator: std.mem.Allocator) !Self {
+    const runfiles_path = try discoverRunfiles(allocator);
 
     var result = Self{
         .directory = runfiles_path,
