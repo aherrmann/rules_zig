97eaf4502e0009c3737f289fc7dcec1489e536b5
Andreas Herrmann <andreash87@gmx.ch>
2024-02-04 19:28:44 +0100

    Test bazel_builtin in package dependency

--- e2e/workspace/bazel_builtin/package.zig
+++ e2e/workspace/bazel_builtin/package.zig
@@ -1,13 +1,8 @@
 const std = @import("std");
 const bazel_builtin = @import("bazel_builtin");
-const package = @import("package");
 
-test "bazel_builtin exposes label" {
-    try std.testing.expectEqualStrings("test", bazel_builtin.current_target);
+pub fn testPackage() !void {
+    try std.testing.expectEqualStrings("package", bazel_builtin.current_target);
     try std.testing.expectEqualStrings("bazel_builtin", bazel_builtin.current_package);
     try std.testing.expectEqualStrings("", bazel_builtin.current_repository);
-}
-
-test "bazel_builtin in package dependency" {
-    try package.testPackage();
 }
