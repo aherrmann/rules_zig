8778a490c5b7d1a7d9351b071f18fe5f6e0c2788
Andreas Herrmann <andreash87@gmx.ch>
2024-02-04 20:45:22 +0100

    zig_runfiles -> bazel_builtin - current_repository
    Replace zig_runfiles by bazel_builtin to determine current_repository.

--- e2e/workspace/runfiles-library/dependency/main.zig
+++ e2e/workspace/runfiles-library/dependency/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const runfiles = @import("runfiles");
-const dependency_runfiles = @import("dependency_runfiles");
+const bazel_builtin = @import("bazel_builtin");
 
 pub fn readData(allocator: std.mem.Allocator) ![]const u8 {
     var r = try runfiles.Runfiles.create(allocator);
@@ -8,7 +8,7 @@ pub fn readData(allocator: std.mem.Allocator) ![]const u8 {
 
     const rpath = "runfiles_library_transitive_dependency/data.txt";
 
-    const file_path = try r.rlocation(allocator, rpath, dependency_runfiles.current_repository);
+    const file_path = try r.rlocation(allocator, rpath, bazel_builtin.current_repository);
     defer allocator.free(file_path);
 
     var file = try std.fs.cwd().openFile(file_path, .{});
