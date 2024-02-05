eeba54c0c84666715f1c3b7c3fdccad7f0ae38ee
Andreas Herrmann <andreash87@gmx.ch>
2024-02-05 08:18:07 +0100

    Integration test for runfiles

---
+++ zig/tests/integration_tests/workspace/runfiles/main.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const runfiles = @import("runfiles");
+const bazel_builtin = @import("bazel_builtin");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    var r = try runfiles.Runfiles.create(allocator);
+    defer r.deinit(allocator);
+
+    const rpath = "__main__/runfiles/data.txt";
+
+    const file_path = try r.rlocation(allocator, rpath, bazel_builtin.current_repository);
+    defer allocator.free(file_path);
+
+    const file = std.fs.cwd().openFile(file_path, .{}) catch |e| {
+        std.log.err("Failed to open file '{s}': {}", .{ file_path, e });
+        return e;
+    };
+    defer file.close();
+
+    const content = try file.readToEndAlloc(allocator, 4096);
+    defer allocator.free(content);
+
+    try std.io.getStdOut().writer().print("data: {s}", .{content});
+}
