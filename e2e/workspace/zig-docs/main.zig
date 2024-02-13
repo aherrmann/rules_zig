6a896c51ecb89b3474e4d6e2f50b667de32e3e74
Andreas Herrmann <andreash87@gmx.ch>
2024-02-13 20:29:29 +0100

    Add a doctest

--- e2e/workspace/zig-docs/main.zig
+++ e2e/workspace/zig-docs/main.zig
@@ -4,7 +4,7 @@
 //!zig-autodoc-guide: ../guide.md
 
 const std = @import("std");
-const hello_world = @import("hello_world");
+pub const hello_world = @import("hello_world");
 
 /// Prints "Hello World!".
 pub fn say_hello_world() !void {
@@ -17,4 +17,9 @@ pub fn say_hello_world() !void {
 /// Prints "Hello World!".
 pub fn main() void {
     say_hello_world() catch unreachable;
+}
+
+test hello_world {
+    // Hello World message.
+    try std.testing.expectEqualStrings("Hello World!", hello_world.msg);
 }
