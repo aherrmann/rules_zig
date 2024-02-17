74a988be09b52a149b617ada1265752ef694a9fb
Andreas Herrmann <andreash87@gmx.ch>
2024-02-17 20:28:50 +0100

    fix: e2e bazel_builtin test

--- e2e/workspace/bazel_builtin/module.zig
+++ e2e/workspace/bazel_builtin/module.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const bazel_builtin = @import("bazel_builtin");
 
 pub fn testModule() !void {
-    try std.testing.expectEqualStrings("package", bazel_builtin.current_target);
+    try std.testing.expectEqualStrings("module", bazel_builtin.current_target);
     try std.testing.expectEqualStrings("bazel_builtin", bazel_builtin.current_package);
     try std.testing.expectEqualStrings("", bazel_builtin.current_repository);
 }
