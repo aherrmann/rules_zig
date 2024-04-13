29487574febd6e2e4e31057fb55a4e9ddf4eeef1
Andreas Herrmann <andreash87@gmx.ch>
2024-04-13 16:55:58 +0200

    Revert "TMP zig_configure_test on dev version"
    This reverts commit 2e79c862b2d2c4652f1dc4812ffe164f23523501.

--- e2e/workspace/configure-version/test-0.11.0-dev.3886+0c1bfe271.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const builtin = @import("builtin");
-
-test "match Zig version" {
-    try std.testing.expectEqualStrings("0.11.0-dev.3886+0c1bfe271", builtin.zig_version_string);
-}
