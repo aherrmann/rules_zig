24391a7b73e7a7caa91c85bb5f3ac264ce32e0ad
Andreas Herrmann <andreash87@gmx.ch>
2025-03-15 11:11:19 +0100

    Fix @export operand is now a pointer
    See https://ziglang.org/download/0.14.0/release-notes.html#export-Operand-is-Now-a-Pointer

--- e2e/workspace/configure-mode/library-0.13.0.zig
+++ e2e/workspace/configure-mode/library-0.13.0.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 comptime {
-    @export(&internalName, .{
+    @export(internalName, .{
         .name = @tagName(builtin.mode),
         .linkage = if (builtin.zig_version.major == 0 and builtin.zig_version.minor == 11)
             .Strong
--- e2e/workspace/configure-threaded/library.zig
+++ e2e/workspace/configure-threaded/library.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 comptime {
-    @export(internalName, .{
+    @export(&internalName, .{
         .name = if (builtin.single_threaded) "single_threaded" else "multi_threaded",
         .linkage = if (builtin.zig_version.major == 0 and builtin.zig_version.minor == 11)
             .Strong
