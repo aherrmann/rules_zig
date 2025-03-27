c1b273a43fc07bbb3a8466374d37dd0625373bf9
Andreas Herrmann <andreash87@gmx.ch>
2025-03-27 06:56:42 +0100

    change workdir for repro

--- e2e/workspace/repro.sh
+++ e2e/workspace/repro.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE=/home/aj/.cache/bazel/_bazel_aj/ffffacc6b31c04d73393a64a0e6bdb65
 
 zig_docs() {
   IDX="$1"
   VERSION="$2"
   KIND="$3"
-  OUT="repro-out/$IDX-$VERSION"
-  mkdir -p "$OUT"
+  WD="repro-wd/$IDX-$VERSION"
   case $KIND in
     binary) FLAGS=(build-exe);;
     library) FLAGS=(build-lib);;
     test) FLAGS=(test --test-no-exec);;
   esac
-  "$BASE/external/rules_zig++zig+zig_${VERSION}_x86_64-linux/zig" "${FLAGS[@]}" \
-    -femit-docs="$OUT/test.docs" \
-    -fno-emit-bin \
-    -fno-emit-implib \
-    --zig-lib-dir "$BASE/external/rules_zig++zig+zig_${VERSION}_x86_64-linux/lib" \
-    --cache-dir "/tmp/zig-cache" \
-    --global-cache-dir "/tmp/zig-cache" \
-    -Mtest=zig-docs/main.zig
+  mkdir -p "$WD"
+  (
+    cd "$WD"
+    "$BASE/external/rules_zig++zig+zig_${VERSION}_x86_64-linux/zig" "${FLAGS[@]}" \
+      -femit-docs="out.docs" \
+      -fno-emit-bin \
+      -fno-emit-implib \
+      --zig-lib-dir "$BASE/external/rules_zig++zig+zig_${VERSION}_x86_64-linux/lib" \
+      --cache-dir "/tmp/zig-cache" \
+      --global-cache-dir "/tmp/zig-cache" \
+      -Mtest="../../zig-docs/main.zig"
+  )
+  rm -rf $WD
 }
 
-rm -rf /tmp/zig-cache repro-out
+rm -rf /tmp/zig-cache repro-wd
 for i in `seq 10`; do
   for kind in binary library test; do
     zig_docs $i 0.14.0 $kind &
