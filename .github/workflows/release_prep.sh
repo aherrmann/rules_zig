aherrmann/rules_zig

commit 2b734f960ba5ac008570d40bfa32dfacf7c3ecf0

Remove WORKSPACE mode from release notes

.github/workflows/release_prep.sh
@@ -6,36 +6,24 @@ set -o errexit -o nounset -o pipefail
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 VERSION=${TAG:1}
-# The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_zig-$VERSION"
-ARCHIVE="$PREFIX.tar.gz"
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
-SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel >=6
+## Setup Instructions
 
-1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
+Add the following to your \`MODULE.bazel\` file to install rules_zig:
 
 \`\`\`starlark
 bazel_dep(name = "rules_zig", version = "$VERSION")
 \`\`\`
 
-## Using WORKSPACE
-
-Paste this snippet into your \`WORKSPACE.bazel\` file:
+Optionally add the following to your \`MODULE.bazel\` file to install a specific Zig toolchain version:
 
 \`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+zig = use_extension("//zig:extensions.bzl", "zig")
+zig.toolchain(zig_version = "0.13.0")
+\`\`\`
 
-http_archive(
-    name = "rules_zig",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/aherrmann/rules_zig/releases/download/${TAG}/${ARCHIVE}",
-)
+You can call \`zig.toolchain\` multiple times to install multiple Zig versions.
+
+Note, rules_zig requires bzlmod, WORKSPACE mode is no longer supported.
 EOF
-
-awk 'f;/--SNIP--/{f=1}' e2e/workspace/WORKSPACE.bazel
-echo "\`\`\`"

commit 52df1e6d9ad95f4a4c4ee3abd9c72395bd1153d2

Fix release preparation script, broken in 2b734f9

.github/workflows/release_prep.sh
@@ -6,6 +6,10 @@ set -o errexit -o nounset -o pipefail
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 VERSION=${TAG:1}
+# The prefix is chosen to match what GitHub generates for source archives
+PREFIX="rules_zig-$VERSION"
+ARCHIVE="$PREFIX.tar.gz"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 
 cat << EOF
 ## Setup Instructions