c929516a605a592b470cd14fb324252943e71fd9
Andreas Herrmann <andreash87@gmx.ch>
2026-04-28 09:35:24 +0200

    restore zig-docs test
    It is not automatically tested, but, the purpose of those tests is to
    check that docs generation works properly, and the source doc strings
    make their way into the generated docs.

--- e2e/workspace/zig-docs/main.zig
+++ e2e/workspace/zig-docs/main.zig
@@ -1,18 +1,35 @@
 //!zig-autodoc-guide: guide.md
 
+// NOTE: zig-autodoc-guide not supported as of Zig 0.14+,
+// see https://ziggit.dev/t/zig-autodoc-render-markdown-files/10314/2
+
 const builtin = @import("builtin");
 const std = @import("std");
 pub const hello_world = @import("hello_world");
 
 const is_zig_0_16_or_later = builtin.zig_version.major == 0 and builtin.zig_version.minor >= 16;
-pub const main = if (is_zig_0_16_or_later) main_016 else main_pre_016;
 
-fn main_pre_016() void {
+/// Prints "Hello World!".
+pub const say_hello_world = if (is_zig_0_16_or_later) say_hello_world_016 else say_hello_world_pre_016;
+
+fn say_hello_world_016(io: anytype) !void {
+    std.Io.File.writeStreamingAll(.stdout(), io, hello_world.msg ++ "\n") catch unreachable;
+}
+
+fn say_hello_world_pre_016() !void {
     std.fs.File.stdout().writeAll(hello_world.msg ++ "\n") catch unreachable;
 }
 
+/// Program entry-point.
+/// Prints "Hello World!".
+pub const main = if (is_zig_0_16_or_later) main_016 else main_pre_016;
+
+fn main_pre_016() void {
+    say_hello_world_pre_016() catch unreachable;
+}
+
 fn main_016(init: std.process.Init) void {
-    std.Io.File.writeStreamingAll(.stdout(), init.io, hello_world.msg ++ "\n") catch unreachable;
+    say_hello_world(init.io) catch unreachable;
 }
 
 test hello_world {
